28c5d3f2a8fbeef1d4e62046870aac073dafb9d1
chendi <bbigcd@163.com>
2016-11-05 15:24:14 +0800

    ble background test demo
    a demo that test CBCentralManager scan peripheral in background

--- BLEBackgroundTest/ViewController.m
+++ BLEBackgroundTest/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface ViewController ()
+@interface ViewController ()<CBCentralManagerDelegate, CBPeripheralDelegate>
+
+@property (nonatomic, strong) CBCentralManager *manger;
 
 @end
 
@@ -16,12 +19,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.manger = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:@{CBCentralManagerOptionRestoreIdentifierKey:@"restoreIdentifier"}];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central{
+    if (central.state == CBPeripheralManagerStatePoweredOn) {
+        [_manger scanForPeripheralsWithServices:@[[CBUUID UUIDWithString:@"FEE7"], [CBUUID UUIDWithString:@"FFFF"], [CBUUID UUIDWithString:@"FFC0"]] options:nil];
+        NSLog(@"open bluetooth");
+    }
 }
+
+- (void)centralManager:(CBCentralManager *)central willRestoreState:(NSDictionary<NSString *, id> *)dict{
+    
+}
+
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *, id> *)advertisementData RSSI:(NSNumber *)RSSI{
+    NSLog(@"%@", advertisementData);
+}
+
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral{
+    
+}
+
 
 @end
